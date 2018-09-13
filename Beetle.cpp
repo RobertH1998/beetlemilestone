11945a05f4a9713526d7338c05f0690e3d29b3ca
RobertH1998 <35371517+RobertH1998@users.noreply.github.com>
2018-09-13 19:49:26 -0400

    Create Beetle.cpp

---
+++ Beetle.cpp
@@ -0,0 +1,68 @@
+// Robert Hogsed Beetle game
+
+#include <iostream>
+using namespace std;
+
+class Beetle {
+public:
+
+	//Beetle_part_check{
+
+	//};
+
+	//Add_beetle_part{
+
+	//};
+
+	//Beetle_complete{
+
+	//};
+
+//};
+
+
+
+
+int main() {
+	Beetle Beetle_part_check;
+	Beetle Add_beetle_Part;
+	Beetle Beetle_Complete;
+
+
+
+	};
+	return 0;
+// Robert Hogsed Beetle game
+
+#include <iostream>
+using namespace std;
+
+class Beetle {
+public:
+
+	//Beetle_part_check{
+
+	//};
+
+	//Add_beetle_part{
+
+	//};
+
+	//Beetle_complete{
+
+	//};
+
+//};
+
+
+
+
+int main() {
+	Beetle Beetle_part_check;
+	Beetle Add_beetle_Part;
+	Beetle Beetle_Complete;
+
+
+
+	};
+	return 0;
